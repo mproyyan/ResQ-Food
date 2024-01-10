dbfb9ec26a96c1c8ff1bbe444cdf02daaf72260c
mproyyan <mproyyan@gmail.com>
2024-01-10 22:09:58 +0700

    setup vue filepond

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import envFile from "vite-plugin-env-file";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    envFile({
+      filename: ".env",
+      encrypt: true,
+    }),
   ],
   resolve: {
     alias: {
